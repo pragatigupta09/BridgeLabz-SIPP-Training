13b65af92777af45c7bb332c08c893b05f47be04
pragatigupta09 <pragatigp06@gmail.com>
2025-06-12 14:01:46 +0530

    [Pragati Gupta] Added height converter program

---
+++ Day1_ProgrammingElements/Level-1/HeightConverter.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class HeightConverter  {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your height in centimeters: ");
+        double cm = input.nextDouble();
+
+        double totalInches = cm / 2.54;
+        int feet = (int) (totalInches / 12);
+        double inches = totalInches % 12;
+
+        System.out.println("Your height in cm is " + cm + " while in feet is " + feet + " and inches is " + inches);
+    }
+}
