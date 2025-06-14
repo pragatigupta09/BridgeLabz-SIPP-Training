86c02cf1e5cc406da06262640ab784b27ea2a9fc
pragatigupta09 <pragatigp06@gmail.com>
2025-06-14 19:28:43 +0530

    [Pragati Gupta] Added factorial using for loop program

---
+++ ControlFlows/Level-1/FactorialFor.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FactorialFor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int n = input.nextInt();
+
+        if (n >= 0) {
+            long fact = 1;
+            for (int i = 1; i <= n; i++) {
+                fact *= i;
+            }
+            System.out.println("Factorial of " + n + " is " + fact);
+        } else {
+            System.out.println("Please enter a non-negative integer.");
+        }
+    }
+}
