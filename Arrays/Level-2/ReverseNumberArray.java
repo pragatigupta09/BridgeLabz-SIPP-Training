d09784edef28d9efdbad3df85d85ff93072c34c5
pragatigupta09 <pragatigp06@gmail.com>
2025-06-16 11:59:11 +0530

    [Pragati Gupta] Added store elements in reverse order in array program

---
+++ Arrays/Level-2/ReverseNumberArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ReverseNumberArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int temp = number, count = 0;
+
+        while (temp != 0) {
+            temp /= 10;
+            count++;
+        }
+
+        int[] digits = new int[count];
+        for (int i = 0; i < count; i++) {
+            digits[i] = number % 10;
+            number /= 10;
+        }
+
+        System.out.print("Reversed number using array: ");
+        for (int i = 0; i < count; i++) {
+            System.out.print(digits[i]);
+        }
+    }
+}
