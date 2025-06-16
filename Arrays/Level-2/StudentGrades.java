fa9075482f67c724a41ac04e5cfcb3d917fde4ef
pragatigupta09 <pragatigp06@gmail.com>
2025-06-16 12:28:27 +0530

    [Pragati Gupta] Added students grades program

---
+++ Arrays/Level-2/StudentGrades.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class StudentGrades {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of students: ");
+        int n = sc.nextInt();
+
+        int[][] marks = new int[n][3]; 
+        double[] percentage = new double[n];
+        char[] grade = new char[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nEnter marks for Student " + (i + 1) + ":");
+            for (int j = 0; j < 3; j++) {
+                String subject = j == 0 ? "Physics" : (j == 1 ? "Chemistry" : "Maths");
+                do {
+                    System.out.print("  Enter " + subject + " marks (0-100): ");
+                    marks[i][j] = sc.nextInt();
+                    if (marks[i][j] < 0 || marks[i][j] > 100) {
+                        System.out.println("  Invalid marks. Please enter between 0 and 100.");
+                    }
+                } while (marks[i][j] < 0 || marks[i][j] > 100);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int total = marks[i][0] + marks[i][1] + marks[i][2];
+            percentage[i] = total / 3.0;
+
+            if (percentage[i] >= 80) {
+                grade[i] = 'A';
+            } else if (percentage[i] >= 70) {
+                grade[i] = 'B';
+            } else if (percentage[i] >= 60) {
+                grade[i] = 'C';
+            } else if (percentage[i] >= 50) {
+                grade[i] = 'D';
+            } else if (percentage[i] >= 40) {
+                grade[i] = 'E';
+            } else {
+                grade[i] = 'R';
+            }
+        }
+
+        System.out.println("\n Student Report ");
+        System.out.printf("%-10s %-10s %-10s %-12s %-10s %-10s\n", "Physics", "Chemistry", "Maths", "Percentage", "Grade", "Remarks");
+        for (int i = 0; i < n; i++) {
+            String remark = switch (grade[i]) {
+                case 'A' -> "Level 4 (Excellent)";
+                case 'B' -> "Level 3 (Good)";
+                case 'C' -> "Level 2 (Average)";
+                case 'D' -> "Level 1 (Below Avg)";
+                case 'E' -> "Level 1- (Low)";
+                case 'R' -> "Remedial";
+                default -> "N/A";
+            };
+
+            System.out.printf("%-10d %-10d %-10d %-12.2f %-10c %-10s\n",
+                    marks[i][0], marks[i][1], marks[i][2], percentage[i], grade[i], remark);
+        }
+    }
+}
