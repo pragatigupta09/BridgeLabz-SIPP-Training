5defa2b570b6e26bce4acbf82dbb9039d0aca5dd
pragatigupta09 <pragatigp06@gmail.com>
2025-06-19 22:05:56 +0530

    [Pragati Gupta] Added voting eligibility program

---
+++ Strings/Level-2/VotingEligibility.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        int[] ages = generateRandomAges(n);
+        String[][] results = evaluateVotingEligibility(ages);
+        System.out.println("\nAge\tCan Vote?");
+        displayTable(results);
+    }
+
+    public static int[] generateRandomAges(int n) {
+        int[] ages = new int[n];
+        for (int i = 0; i < n; i++) {
+            ages[i] = (int)(Math.random() * 100);
+        }
+        return ages;
+    }
+
+    public static String[][] evaluateVotingEligibility(int[] ages) {
+        String[][] result = new String[ages.length][2];
+        for (int i = 0; i < ages.length; i++) {
+            result[i][0] = String.valueOf(ages[i]);
+            if (ages[i] < 0) {
+                result[i][1] = "false";
+            } else if (ages[i] >= 18) {
+                result[i][1] = "true";
+            } else {
+                result[i][1] = "false";
+            }
+        }
+        return result;
+    }
+
+    public static void displayTable(String[][] data) {
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i][0] + "\t" + data[i][1]);
+        }
+    }
+}
