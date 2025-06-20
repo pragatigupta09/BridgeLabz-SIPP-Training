471c9de5305d8826090131df2a9b8a0dfc57f344
pragatigupta09 <pragatigp06@gmail.com>
2025-06-20 19:28:57 +0530

    [Pragati Gupta] Added check prime program

---
+++ Built-In-Function/PrimeCheck.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        boolean prime = isPrime(num);
+        System.out.println(num + " is " + (prime ? "prime" : "not prime"));
+        sc.close();
+    }
+
+    static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++)
+            if (num % i == 0) return false;
+        return true;
+    }
+}
