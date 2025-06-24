a9ac55478745aa37df228d1294e07d2054ecc9b4
pragatigupta09 <pragatigp06@gmail.com>
2025-06-24 23:18:07 +0530

    [Pragati Gupta] Added hotel booking program

---
+++ Constructors/Level-1/HotelBooking.java
@@ -0,0 +1,25 @@
+class HotelBooking {
+    String guestName;
+    String roomType;
+    int nights;
+
+    HotelBooking() {
+        this("Guest", "Standard", 1);
+    }
+
+    HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    HotelBooking(HotelBooking other) {
+        this.guestName = other.guestName;
+        this.roomType = other.roomType;
+        this.nights = other.nights;
+    }
+
+    void display() {
+        System.out.println("Guest: " + guestName + ", Room: " + roomType + ", Nights: " + nights);
+    }
+}
