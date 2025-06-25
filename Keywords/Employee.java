94583a4422dbece8cb9f46f1d2f08d1e62ef94e7
pragatigupta09 <pragatigp06@gmail.com>
2025-06-25 19:32:38 +0530

    [Pragati Gupta] Added employee management system program

---
+++ Keywords/Employee.java
@@ -0,0 +1,27 @@
+package Keywords;
+
+class Employee {
+    static String companyName = "Infosys";
+    static int totalEmployees = 0;
+
+    final int id;
+    String name, designation;
+
+    Employee(String name, int id, String designation) {
+        this.name = name;
+        this.id = id;
+        this.designation = designation;
+        totalEmployees++;
+    }
+
+    static void displayTotalEmployees() {
+        System.out.println("Total Employees: " + totalEmployees);
+    }
+
+    void display() {
+        if (this instanceof Employee) {
+            System.out.println("Name: " + name + ", ID: " + id + ", Designation: " + designation);
+        }
+    }
+}
+
